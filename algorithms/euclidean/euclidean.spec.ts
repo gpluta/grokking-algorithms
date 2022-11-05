94cb5fc74ccd9dbf0dc489288f15e81728363b32
Grzesiek Pluta <pluta7@gmail.com>
2022-11-05 22:42:14 +0100

    euclidean update

--- algorithms/euclidean/euclidean.spec.ts
+++ algorithms/euclidean/euclidean.spec.ts
@@ -1,35 +1,43 @@
 import { euclidean } from './euclidean'
 
 describe('euclidean test', () => {
-  it('test x=0 y=1', () => {
-    expect(euclidean(0, 1)).toEqual(null)
+  it('test a=0 b=0', () => {
+    expect(euclidean(0, 0)).toEqual(0)
   })
 
-  it('test x=1 y=1', () => {
+  it('test a=0 b=1', () => {
+    expect(euclidean(0, 2)).toEqual(2)
+  })
+
+  it('test a=1 b=0', () => {
+    expect(euclidean(3, 0)).toEqual(3)
+  })
+
+  it('test a=1 b=1', () => {
     expect(euclidean(1, 1)).toEqual(1)
   })
 
-  it('test x=7 y=7', () => {
+  it('test a=7 b=7', () => {
     expect(euclidean(7, 7)).toEqual(7)
   })
 
-  it('test x=16 y=5', () => {
+  it('test a=16 b=5', () => {
     expect(euclidean(16, 5)).toEqual(1)
   })
 
-  it('test x=282 y=78', () => {
+  it('test a=282 b=78', () => {
     expect(euclidean(282, 78)).toEqual(6)
   })
 
-  it('test x=282 y=78', () => {
+  it('test a=282 b=78', () => {
     expect(euclidean(282, 78)).toEqual(6)
   })
 
-  it('test x=78 y=282', () => {
+  it('test a=78 b=282', () => {
     expect(euclidean(78, 282)).toEqual(6)
   })
 
-  it('test x=-15 y=5', () => {
+  it('test a=-15 b=5', () => {
     expect(euclidean(-15, 5)).toEqual(5)
   })
 })
--- algorithms/euclidean/euclidean.ts
+++ algorithms/euclidean/euclidean.ts
@@ -1,6 +1,14 @@
-export function euclidean(a: number, b: number): number | null {
-  if ([a, b].includes(0)) {
-    return null
+export function euclidean(a: number, b: number): number {
+  if (a === 0 && b === 0) {
+    return 0
+  }
+
+  if (a === 0) {
+    return b
+  }
+
+  if (b === 0) {
+    return a
   }
 
   let reminder = a % b
